e51fd44c00dab67d66d051bc03430b73e8fb109a
Kaushik20_20 <debnathkaushik46@gmail.com>
2023-03-01 18:55:39 +0530

    last fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,6 @@ export default async function RootLayout({
     <html>
       <head />
       <body>
-        <SessionProvider session={session}>
           {!session ?(
             <Login/>
           ):(
@@ -28,7 +27,6 @@ export default async function RootLayout({
             <div>{children}</div>
           </div>
           )}
-        </SessionProvider>
       </body>
     </html>
   )
