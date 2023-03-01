96a5714a517ba998d66c37270f0eb4a2b0cd8f1e
Kaushik20_20 <debnathkaushik46@gmail.com>
2023-03-01 18:32:22 +0530

    last fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  target: 'static',
+  ssr: 'false',
+  generate:{
+    fallback: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   experimental:{
