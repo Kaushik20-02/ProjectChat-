3f89302408ed701220737a8ecff7151bbbe7e8f6
Kaushik20_20 <debnathkaushik46@gmail.com>
2023-05-11 11:45:13 +0530

    Add existing project files to Git

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -23,10 +23,10 @@ const TopBar = () => {
       alt='Profile' className="w-12 h-12 rounded-full
       m-2 hover:scale-90 cursor-pointer"/>}
 
-    <div className='dark:text-slate-200'
+   {/* <div className='dark:text-slate-200'
          onClick={()=> setTheme(nextTheme)}>
           <RiMoonFoggyLine className='cursor-pointer w-6 h-6'/></div>
-      </div>
+ */} </div>
     </div>
 
   )
